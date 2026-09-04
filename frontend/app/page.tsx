1e666f50da701301ed7b07a3995f771c11d7e0a5
notcoinsapourt-web <notcoinsapourt@gmail.com>
2026-09-04 15:54:07 +0330

    Use modular storefront v4 shell

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,9 @@
-import Storefront from "../components/Storefront";
+import StoreShell from "../components/store/StoreShell";
 import { getStoreData } from "../lib/store-data";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
   const data = await getStoreData();
-  return <Storefront data={data} />;
+  return <StoreShell data={data} />;
 }
