a5e39886a607232e4a90892190fca0cdc53c76cd
notcoinsapourt-web <notcoinsapourt@gmail.com>
2026-09-04 17:21:58 +0330

    Route offline site to branded maintenance experience

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,4 @@
+import MaintenancePage from "../components/store/MaintenancePage";
 import StoreShell from "../components/store/StoreShell";
 import { getStoreData } from "../lib/store-data";
 
@@ -5,5 +6,8 @@ export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
   const data = await getStoreData();
+  if (data.settings.websiteMaintenance) {
+    return <MaintenancePage shopName={data.settings.shopName} supportUsername={data.settings.supportUsername || "@Znoxe_shope"}/>;
+  }
   return <StoreShell data={data} />;
 }
